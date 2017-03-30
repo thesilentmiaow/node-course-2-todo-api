0d8977c432834d347773976fe0d1e484cef0b86b
Timothy Hodges <timhodges@Timothys-MBP.lan>
2017-03-30 18:45:13 +0100

    Fix bug for body-parser name

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var  {mongoose} =require ('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+
 var app = express();
 const port = process.env.PORT || 3000;
 
